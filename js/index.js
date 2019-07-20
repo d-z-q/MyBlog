b23ad4dba83c47677658dc74ee086b4f44eef77b
d-z-q <github192018>
2019-07-21 00:59:07 +0800

    原生js
    菜单栏切换效果和瀑布流实现

---
+++ js/index.js
@@ -0,0 +1,13 @@
+;(function(){
+	var obj=null;
+	var As=document.getElementById('top-nav').getElementsByTagName('a');
+	obj = As[0];
+	for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
+	obj=As[i];}
+	obj.id='nava'
+})()
+
+;(function(){
+	
+	
+})()
